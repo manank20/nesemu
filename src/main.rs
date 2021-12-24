fb7f66185d9f6171ba5f3b63f4650719beb6647c
Manank Patel <f20201696@goa.bits-pilani.ac.in>
2021-12-24 18:51:54 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+
+
+pub struct CPU  {
+    pub register_a: u8,
+    pub status: u8,
+    pub program_counter: u16,
+}
+
+impl CPU {
+    pub fn new() -> Self {
+        CPU {
+            register_a: 0,
+            status: 0,
+            program_counter: 0,
+        }
+    }
+
+
+    pub fn interpret(&mut self, program: Vec<u8>) {
+        self.program_counter= 0;
+
+        loop {
+            let opcode = program[self.program_counter as usize];
+            self.program_counter += 1;
+
+            match opcode {
+                0xA9 => {
+                    let param = program[self.program_counter as usize];
+                    self.program_counter += 1;
+                    self.register_a = param;
+
+                    if self.register_a == 0 {
+                        self.status = self.status | 0b0000_0010;
+                    }  else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    if self.register_a & 0b1000_0000 != 0 {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                    self.status = self.status & 0b111_1111
+                    }
+                }
+
+
+                _ => todo!()
+                
+            }
+        }
+    }
+    
+}
+
+
+
+fn main() {
+
+}
